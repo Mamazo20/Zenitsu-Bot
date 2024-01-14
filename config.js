6a6a991013d5cc9a87b6d19733cd22328494c837
Mamazo20 <156611408+Mamazo20@users.noreply.github.com>
2024-01-14 17:21:06 -0300

    Update config.js

--- config.js
+++ config.js
@@ -10,9 +10,9 @@ global.antitags = true
 //global.autoTyping = false //auto tying in gc (true to on, false to off)
 //global.autoRecord = true //auto recording (true to on, false to off)
 
-global.Owner = ['918348225320'] //ur number 
-global.OwnerNumber = ['918348225320']
-global.ownertag = ['918348225320']
+global.Owner = ['5564935007742'] //ur number 
+global.OwnerNumber = ['5564993258937']
+global.ownertag = ['5564993258937']
 global.BotName = "ZenitsuBot" //ur bot name 
 global.packname = "ZenitsuBot"
 global.author = "🦄Dream Guy Deepak" //ur sticker pack name 
